0405d1e2795d902c6f7aee97b83681dbdfc93816
B3z3rk <101910007+B3z3rk@users.noreply.github.com>
2022-12-01 12:35:52 -0500

    Edited Files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Menu toggle----------------------------------------------
 var MenuItems = document.getElementById("MenuItems");
 
     MenuItems.style.maxHeight = "0px";
@@ -12,3 +14,58 @@ var MenuItems = document.getElementById("MenuItems");
             MenuItems.style.maxHeight = "0px";
         }
     }
+
+    // js for product gallery-------------------------------
+
+    var productImg = document.getElementById("productImg")
+    var smallImg = document.getElementsByClassName("small-img")
+
+        smallImg[0].onclick = function(){
+            productImg.src = smallImg[0].src;
+        }
+        smallImg[1].onclick = function(){
+            productImg.src = smallImg[1].src;
+        }
+        smallImg[2].onclick = function(){
+            productImg.src = smallImg[2].src;
+        }
+        smallImg[3].onclick = function(){
+            productImg.src = smallImg[3].src;
+        }
+
+
+
+    // Cart function-------------------------------------------------------
+    var parent= document.querySelector(".modal-parent")
+    var productbtn = document.getElementsByClassName('addp');
+    var x = document.querySelector(".X");
+
+    for (var i = 0; i < productbtn.length ; i++){
+        var button = productbtn[i];
+        button.addEventListener("click", appear);
+    }
+ 
+    
+    function appear() {
+        console.log("click");
+        parent.style.display = "block";
+    }
+    
+    x.addEventListener("click", disappearX);
+    function disappearX() {
+      parent.style.display = "none";
+    }
+    
+    parent.addEventListener("click", disappearParent)
+    function disappearParent(e) {
+      if (e.target.className == "modal-parent") {
+        parent.style.display = "none";
+      }
+    }
+var addtocart = document.getElementById("cart2");
+
+addtocart.addEventListener("click", add);
+
+function add(){
+    
+}
