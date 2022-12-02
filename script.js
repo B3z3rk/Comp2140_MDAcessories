b2af10e113702962a4715be49394b38817e2d196
B3z3rk <101910007+B3z3rk@users.noreply.github.com>
2022-12-02 16:20:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,227 @@
-var selectedRow = null
+// Menu toggle----------------------------------------------
+var MenuItems = document.getElementById("MenuItems");
 
-function onFormSubmit(e) {
-	event.preventDefault();
-        var formData = readFormData();
-        if (selectedRow == null){
-            insertNewRecord(formData);
-		}
-        else{
-            updateRecord(formData);
-		}
-        resetForm();    
-}
+MenuItems.style.maxHeight = "0px";
 
-//Retrieve the data
-function readFormData() {
-    var formData = {};
-    formData["productCode"] = document.getElementById("productCode").value;
-    formData["product"] = document.getElementById("product").value;
-    formData["qty"] = document.getElementById("qty").value;
-    formData["perPrice"] = document.getElementById("perPrice").value;
-    return formData;
-}
+function menutoggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+MenuItems.style.maxHeight = "0px";
 
-//Insert the data
-function insertNewRecord(data) {
-    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-		cell1.innerHTML = data.productCode;
-    cell2 = newRow.insertCell(1);
-		cell2.innerHTML = data.product;
-    cell3 = newRow.insertCell(2);
-		cell3.innerHTML = data.qty;
-    cell4 = newRow.insertCell(3);
-		cell4.innerHTML = data.perPrice;
-    cell4 = newRow.insertCell(4);
-        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
-}
+function menutoggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+    MenuItems.style.maxHeight = "0px";
 
-//Edit the data
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.productCode;
-    selectedRow.cells[1].innerHTML = formData.product;
-    selectedRow.cells[2].innerHTML = formData.qty;
-    selectedRow.cells[3].innerHTML = formData.perPrice;
-}
-
-//Delete the data
-function onDelete(td) {
-    if (confirm('Do you want to delete this record?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById('storeList').deleteRow(row.rowIndex);
-        resetForm();
+    function menutoggle(){
+        if( MenuItems.style.maxHeight == "0px")
+        {
+            MenuItems.style.maxHeight = "200px";
+        }
+        else
+        {
+            MenuItems.style.maxHeight = "0px";
+        }
     }
+  }
+}
+  }
 }
 
-//Reset the data
-function resetForm() {
-    document.getElementById("productCode").value = '';
-    document.getElementById("product").value = '';
-    document.getElementById("qty").value = '';
-    document.getElementById("perPrice").value = '';
-    selectedRow = null;
-}
+    // js for product gallery-------------------------------
+
+    var productImg = document.getElementById("productImg")
+    var smallImg = document.getElementsByClassName("small-img")
+
+        smallImg[0].onclick = function(){
+            productImg.src = smallImg[0].src;
+        }
+        smallImg[1].onclick = function(){
+            productImg.src = smallImg[1].src;
+        }
+        smallImg[2].onclick = function(){
+            productImg.src = smallImg[2].src;
+        }
+        smallImg[3].onclick = function(){
+            productImg.src = smallImg[3].src;
+        }
+
+
+
+    // Cart function-------------------------------------------------------
+   
+    let products = [
+        {name:"Resin Letter Key Rings",
+        price: 1000,
+        inCart: 0 },
+    
+        {
+            name:"Black Sand Unisex Ring",
+            price: 2500,
+            inCart: 0 }
+    
+            ,{
+                name:"Glow in the Dark Bracelet",
+                price: 1550,
+                inCart: 0 },
+                {
+                    name:"Beaded Earrings",
+                    price: 1500,
+                    inCart: 0 },
+                    {
+                        name:"Unisex Silver Ring",
+                        price: 1000,
+                        inCart: 0 },
+                        {name: "Beaded waistbeads",
+                            price: 3000 ,
+                            inCart: 0 },
+                            {name:"Glow in the Dark Bracelet",
+                                price: 1150,
+                                inCart: 0 }
+                                ,
+                                {name:"Reversible Satin Bonnets",
+                                    price: 1500,
+                                    inCart: 0 },
+                                    {name:"Beaded Drop Earrings",
+                                        price: 3000,
+                                        inCart: 0 },
+                                        {name:"Beaded Anklets",
+                                            price: 1550,
+                                            inCart: 0 },
+                                            {name:"beaded necklaces",
+                                                price: 1500,
+                                                inCart: 0 },
+        
+    ]
+    let carts= document. getElementsByClassName("addp");
+    for(let i=0; i <carts.length;i++){
+        carts[i].addEventListener('click',() =>{
+            cartNumbers(products[i]);
+            totalcost(products[i]);
+        })
+    
+    }
+    function onLoadCartNumbers(){
+        let productNumbers = localStorage.getItem('cartNumbers');
+    
+        if (productNumbers){
+            document.getElementById("quantity").textContent = productNumbers;
+        }
+    }
+    
+    function cartNumbers(product){
+        let productNumbers = localStorage.getItem('cartNumbers');
+        productNumbers = parseInt(productNumbers);
+        if (productNumbers){
+            localStorage.setItem('cartNumbers',productNumbers+1);
+            document.getElementById("quantity").textContent = productNumbers+1;
+        }else{
+            localStorage.setItem('cartNumbers',1);
+            document.getElementById("quantity").textContent = 1;
+        }
+    
+        setItems(product);
+    }
+    
+    function setItems(product){
+        let cartItems= localStorage.getItem('productsInCart');
+        cartItems = JSON.parse(cartItems);
+        if (cartItems!=null){
+            if (cartItems[product.name] == undefined){
+                cartItems = {
+                    ...cartItems, 
+                    [product.name]:product
+    
+                }
+            }
+            cartItems[product.name].inCart+=1;
+        }else{
+            product.inCart = 1;
+            cartItems={
+                [product.name]:product 
+            }
+        }
+        localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    }
+
+    
+    function totalcost(product){
+        let cartcost = localStorage.getItem('totalcost');
+        if (cartcost !=null){
+            cartcost=parseInt(cartcost)
+            localStorage.setItem("totalcost",cartcost+ product.price);
+        }else{
+            localStorage.setItem("totalcost",product.price);
+
+        }
+        
+    }
+
+    let button = document.getElementById("checkout");
+    button.addEventListener('click',checkout);
+    function checkout(){
+        window.open("checkout.html");
+        
+    }
+    function displayCart(){
+        let cartcost = localStorage.getItem('totalcost');
+        let cartItems = localStorage.getItem("productsInCart");
+        cartItems = JSON.parse(cartItems);
+        let productcontainer = document.querySelector(".cart-product")
+        if (cartItems && productcontainer){
+            productcontainer.innerHTML = ' ';
+            Object.values(cartItems).map(item=> {
+                productcontainer.innerHtml += 
+                `<div class = "cart-product">
+                <ion-icon name = "close-circle></ion-icon>
+                <span>${item.name}</span>
+                <div class = "product-price">$${item.price},00</div>
+                <div class = "product-quanity">
+                <ion-icon class= "decrease" name ="arrow-dropleft-circle"><ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class= "increase" name ="arrow-dropright-circle"><ion-icon>
+                </div>
+                <div class= "total">
+                $${item.inCart *item.price},00
+                </div>
+                </div>
+                `;
+            });
+
+            productcontainer.innerHTML += `
+            <div class= "basket-total>
+            <h4 class = "total-title> 
+                total
+            </h>
+            <h4 class = "basketTotal">
+             $${cartcost}
+
+
+            `
+            
+        }
+    }
+    
+
+    
+    onLoadCartNumbers();
+    displayCart();
+    
+// Menu toggle----------------------------------------------
+var MenuItems = document.getElementById("MenuItems");
+
+    MenuItems.style.maxHeight = "0px";
+
+    function menutoggle(){
+        if( MenuItems.style.maxHeight == "0px")
+        {
+            MenuItems.style.maxHeight = "200px";
+        }
+        else
+        {
+            MenuItems.style.maxHeight = "0px";
+        }
+    }
+   
